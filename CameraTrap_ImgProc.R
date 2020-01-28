a27358e4f5aae26c4d3164b4069dc4c30acbcc60
Jane C Ditmer <cowl0037@ad.umn.edu>
2020-01-28 11:51:18 -0600

    corrected IDs from zooniverse download

--- CameraTrap_ImgProc.R
+++ CameraTrap_ImgProc.R
@@ -30,7 +30,7 @@ library(reticulate)
 # py_install("Pillow",envname='py3-virtualenv')
 
 library(tensorflow)
-# install_tensorflow(version = "2.0.0-gpu")
+install_tensorflow(version = "2.0.0-gpu")
 library(keras)
 # install_keras()
 
@@ -62,8 +62,37 @@ rm(metadatasep,imglist,s_c_info,s_c_infosplit)
 # trans <- trans[trans$retired_at!=""&trans$retirement_reason=="consensus",!c("metadata","locations")]
 # trans <- trans[trans$retired_at!="",!c("metadata","locations")]
 
+zoo_dl1 <- fread("C:/Users/cowl0037/Downloads/cedar-creek-eyes-on-the-wild-classifications.csv")
+zoo_dl <- zoo_dl1
+zoo_dl$subject_ids
+length(unique(zoo_dl$subject_ids))
+length((zoo_dl$subject_ids))
 
-ANSWERS <- fread("C:/Users/cowl0037/Downloads/CC_non_aggregated.csv")
+
+zoo_dl$annotations2 <- gsub("\"", "", zoo_dl$annotations )
+
+# annot_split <- strsplit(zoo_dl$annotations3,",")
+
+annot_split2 <- strsplit(zoo_dl$annotations2,",answers:")
+annot_split <- strsplit(sapply(annot_split2,`[`,1),"choice:")
+zoo_dl$speciesID <- sapply(annot_split,`[`,2)
+zoo_dl$speciesID <- tolower(zoo_dl$speciesID)
+zoo_dlTEST <- zoo_dl[1000:20000]
+zoo_dlTEST$created_at
+head(zoo_dlTEST)
+
+
+
+# zoo_dl_SUMMARY <- zoo_dlTEST[, .N, by=.(subject_ids,speciesID)][order(N),]#[.N,]
+zoo_dlTOT <-zoo_dl[, .(sum=length(speciesID)), by=.(subject_ids)]
+SUBJ_CONS <- 
+length(zoo_dlTOT$subject_ids[zoo_dlTOT$sum<3])
+zoo_dl_SUMMARY <- zoo_dl[, .N, by=.(subject_ids,speciesID)][order(-N), .(speciesID=speciesID[1L]), keyby=subject_ids]
+zoo_dl_SUMMARY[zoo_dl_SUMMARY$subject_ids==29335271]
+
+sort(unique(ANSWERS$question__species))
+sort(unique(zoo_dl_SUMMARY$speciesID))
+
 ANSWERS <- fread("~/Downloads/CC_non_aggregated.csv")
 merge.ans <- ANSWERS[,c("season","site","roll","subject_id","question__species")]
 sp.sum <- merge.ans[,.(sum=length(subject_id)),.(question__species)]
@@ -72,15 +101,21 @@ sp.sumord$SPID <- -0:(nrow(sp.sumord)-1)
 merge.ans <- merge(merge.ans,sp.sumord[,-c("sum")],by=c("question__species"))
 
 
+# 
+# info <- merge(trans,merge.ans,by=c("season","site","subject_id"))
+# 
+# 
+# info$BlankorNot <- ifelse(info$question__species=="blank",0,1)
+# info$SimpleID <- ifelse(info$question__species=="blank",0,ifelse(info$question__species=="humanorvehicle",1,2))
+# table(info$question__species)
+# table(info$BlankorNot)
 
-info <- merge(trans,merge.ans,by=c("season","site","subject_id"))
+info <- merge(trans,zoo_dl_SUMMARY,by.x=c("subject_id"),by.y="subject_ids")
 
 
-info$BlankorNot <- ifelse(info$question__species=="blank",0,1)
-info$SimpleID <- ifelse(info$question__species=="blank",0,ifelse(info$question__species=="humanorvehicle",1,2))
-table(info$question__species)
-table(info$BlankorNot)
-
+info$BlankorNot <- ifelse(info$speciesID=="nothingthere",0,1)
+info$SimpleID <- ifelse(info$speciesID=="nothingthere",0,ifelse(info$speciesID=="humanorvehicle",1,2))
+info$SPID <- 999
 
 
 
@@ -91,10 +126,16 @@ table(info$BlankorNot)
 # info$IMG2WLOCATION <- paste(info$season,info$site,info$IMG2NAME,sep="/")
 # info$IMG3WLOCATION <- paste(info$season,info$site,info$IMG3NAME,sep="/")
 
+# names(info)
+# infolong <- melt(info,id.vars=c("season","site","subject_id","question__species","SPID","SimpleID","BlankorNot"),measure.vars=c("IMG1NAME","IMG2NAME","IMG3NAME"),variable.name = "img123",value.name = "ImageName")
+# infolong2 <- melt(info,id.vars=c("season","site","subject_id","question__species","SPID","SimpleID","BlankorNot"),measure.vars=c("IMG1WLOCATION","IMG2WLOCATION","IMG3WLOCATION"),variable.name = "imgnumloc",value.name = "ImageNameWLOCATION")
+# infolong$ImageNameWLOCATION <- infolong2$ImageNameWLOCATION
+
 names(info)
-infolong <- melt(info,id.vars=c("season","site","subject_id","question__species","SPID","SimpleID","BlankorNot"),measure.vars=c("IMG1NAME","IMG2NAME","IMG3NAME"),variable.name = "img123",value.name = "ImageName")
-infolong2 <- melt(info,id.vars=c("season","site","subject_id","question__species","SPID","SimpleID","BlankorNot"),measure.vars=c("IMG1WLOCATION","IMG2WLOCATION","IMG3WLOCATION"),variable.name = "imgnumloc",value.name = "ImageNameWLOCATION")
+infolong <- melt(info,id.vars=c("season","site","subject_id","speciesID","SPID","SimpleID","BlankorNot"),measure.vars=c("IMG1NAME","IMG2NAME","IMG3NAME"),variable.name = "img123",value.name = "ImageName")
+infolong2 <- melt(info,id.vars=c("season","site","subject_id","speciesID","SPID","SimpleID","BlankorNot"),measure.vars=c("IMG1WLOCATION","IMG2WLOCATION","IMG3WLOCATION"),variable.name = "imgnumloc",value.name = "ImageNameWLOCATION")
 infolong$ImageNameWLOCATION <- infolong2$ImageNameWLOCATION
+
 
 # infolong$ImageWithLocation <- paste(infolong$season,infolong$site,infolong$ImageName,sep="/")
 
@@ -145,7 +186,7 @@ imgarray_3a <- as.array(imageData(imgresize_3a))
 
 ##### 3images
 
-imgvect <- as.vector(c(imgarray_1a,imgarray_2a,imgarray_3a))
+imgvect <- as.vector(c(imgarray_1a))#,imgarray_2a,imgarray_3a))
 
 #### motion detection (xdiff)
 oneminustwo <- image_1a-image_2a
@@ -172,8 +213,8 @@ return(list(totdiffvect,info$BlankorNot[info$subject_id==paste(subj_id)][1],subj
 
 
 percentfortraining <- 0.8
-totsubj_list0 <- sample(info$subject_id[info$IMG1WLOCATION%in%imagelist2&info$BlankorNot==0],5000)
-totsubj_list1 <- sample(info$subject_id[info$IMG1WLOCATION%in%imagelist2&info$BlankorNot==1],5000)
+totsubj_list0 <- sample(info$subject_id[info$IMG1WLOCATION%in%imagelist2&info$BlankorNot==0],10000)
+totsubj_list1 <- sample(info$subject_id[info$IMG1WLOCATION%in%imagelist2&info$BlankorNot==1],10000)
 totsubj_list <- c(totsubj_list0,totsubj_list1)
 # totsubj_list <- info$subject_id[info$IMG1WLOCATION%in%imagelist2]
 
@@ -206,15 +247,15 @@ x_test <- do.call(rbind,lapply(test_out,`[[`,4))
 #for use when not doParallel!
 # trainsubj_used <- NULL;xdiff_train <- array(data=NA,dim=c(0));x_train <- array(data=NA,dim=c(0));y_train <- array(data=NA,dim=c(0));system.time(lapply(trainsubj_list,readingroup,"train"))
 
-fwrite(data.frame(trainsubj_used,y_train),"train_info_25Jan2020.csv")
-saveRDS(xdiff_train,"xdiff_train_25Jan2020.Rds")
-saveRDS(x_train,"x_train_25Jan2020.Rds")
+fwrite(data.frame(trainsubj_used,y_train),"train_info_28Jan2020.csv")
+saveRDS(xdiff_train,"xdiff_train_28Jan2020.Rds")
+saveRDS(x_train,"x_train_28Jan2020.Rds")
 
 # testsubj_used <- NULL;xdiff_test <- array(data=NA,dim=c(0));x_test <- array(data=NA,dim=c(0));y_test <- array(data=NA,dim=c(0));system.time(lapply(testsubj_list,readingroup,"test"))
 # 
-fwrite(data.frame(testsubj_used,y_test),"test_info_25Jan2020.csv")
-saveRDS(xdiff_test,"xdiff_test_25Jan2020.Rds")
-saveRDS(x_test,"x_test_25Jan2020.Rds")
+fwrite(data.frame(testsubj_used,y_test),"test_info_28Jan2020.csv")
+saveRDS(xdiff_test,"xdiff_test_28Jan2020.Rds")
+saveRDS(x_test,"x_test_28Jan2020.Rds")
 
 
 
@@ -229,14 +270,14 @@ y_test <- to_categorical(y_test,num_classes = 2)
 #######################################
 
 ##### read in what I made 25 Jan 2020
-train_info <- read.csv("train_info_25Jan2020.csv")
-y_train <- to_categorical(train_info[,2])
-xdiff_train <- readRDS("xdiff_train_25Jan2020.Rds")
-x_train <- readRDS("x_train_25Jan2020.Rds")
-test_info <- read.csv("test_info_25Jan2020.csv")
-y_test <- to_categorical(test_info[,2])
-xdiff_test <- readRDS("xdiff_test_25Jan2020.Rds")
-x_test <- readRDS("x_test_25Jan2020.Rds")
+# train_info <- read.csv("train_info_25Jan2020.csv")
+# y_train <- to_categorical(train_info[,2])
+# xdiff_train <- readRDS("xdiff_train_25Jan2020.Rds")
+# x_train <- readRDS("x_train_25Jan2020.Rds")
+# test_info <- read.csv("test_info_25Jan2020.csv")
+# y_test <- to_categorical(test_info[,2])
+# xdiff_test <- readRDS("xdiff_test_25Jan2020.Rds")
+# x_test <- readRDS("x_test_25Jan2020.Rds")
 
 
 
@@ -262,16 +303,30 @@ history <- model_xdiff %>% fit(
   validation_split = 0.2
 )
 
+#### save this!
+# save_model_tf(model_xdiff,"blankornot_model_28Jan")
+# save_model_hdf5(model_xdiff,"blankornot_model_28Jan.h5")
+
+
 model_xdiff %>% evaluate(xdiff_test, y_test,verbose = 0)
 preds<- model_xdiff %>% predict_classes(xdiff_test)
 table(y_test[,2],preds)
 confusion <- cbind(y_test[,2],preds)
 whicharewrong <- as.data.frame(cbind(confusion,testsubj_used))
+#whicharezero <- as.data.frame(cbind(confusion,testsubj_used))
+ 
+# subj_passalong <- whicharezero$testsubj_used[whicharezero$preds==0]
+# 
+# registerDoParallel(cores=detectCores()-1)
+# system.time(test_out_0s <- foreach(i=subj_passalong,.packages = c("EBImage")) %dopar% readingroup(i))
+# xdiff_test_0s <- do.call(rbind,lapply(test_out_0s,`[[`,1))
+# y_test_0s <- unlist(lapply(test_out_0s,`[[`,2))
+# testsubj_used_0s <- unlist(lapply(test_out_0s,`[[`,3))
+# x_test_0s <- do.call(rbind,lapply(test_out_0s,`[[`,4))
+# y_test_0s <- to_categorical(y_test_0s,num_classes = 2)
+# 
 whicharewrong$testsubj_used[whicharewrong$V1!=whicharewrong$preds]
 
-info$IMG1NAME[info$subject_id%in%whicharewrong$testsubj_used[whicharewrong$V1!=whicharewrong$preds]]
-table(info$site[info$subject_id%in%totsubj_list])
-totsubj_list
 
 
 
@@ -281,9 +336,10 @@ totsubj_list
 
 
 
+### input_shape should be  3*pixelresize*pixelresize if i input all 3
 model <- keras_model_sequential() 
 model %>% 
-  layer_dense(units = 256, activation = "relu", input_shape = c(3*pixelresize*pixelresize)) %>% 
+  layer_dense(units = 256, activation = "relu", input_shape = c(pixelresize*pixelresize)) %>% 
   layer_dropout(rate = 0.4) %>% 
   layer_dense(units = 128, activation = "relu") %>%
   layer_dropout(rate = 0.3) %>%
@@ -300,7 +356,20 @@ history <- model %>% fit(
   epochs = 30, batch_size = 128, 
   validation_split = 0.2
 )
-model %>% predict_classes(x_train)
-y_train[,2]
-y_t_2 <- ifelse(y_train[,2]==0,1,2)
-table(y_t_2,model %>% predict_classes(x_train))
+
+model %>% evaluate(x_test, y_test,verbose = 0)
+preds_still<- model %>% predict_classes(x_test)
+table(y_test[,2],preds_still)
+confusion <- cbind(y_test[,2],preds_still)
+confusion_all <- data.frame(cbind(y_test[,2],preds,preds_still))
+confusion_all$pred_cons <- ifelse(confusion_all$preds==1,1,ifelse(confusion_all$preds_still==1,1,0))
+
+table(confusion_all$V1,confusion_all$pred_cons)
+head(confusion_all,30)
+table(preds,preds_still,y_test[,2])
+
+
+whicharewrong <- as.data.frame(cbind(confusion_all,testsubj_used))
+whicharewrong$testsubj_used[whicharewrong$V1==1&whicharewrong$pred_cons==0]
+info$IMG1NAME[info$subject_id%in%whicharewrong$testsubj_used[whicharewrong$V1==1&whicharewrong$pred_cons==0]]
+info$question__species[info$subject_id%in%whicharewrong$testsubj_used[whicharewrong$V1==1&whicharewrong$pred_cons==0]]
