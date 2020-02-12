e53a39a7400466528fe7d6284c42078c63f566a6
Jane C Ditmer <cowl0037@ad.umn.edu>
2020-02-12 11:29:10 -0600

    fixing even more names

--- Exif_merge.R
+++ Exif_merge.R
@@ -63,13 +63,45 @@ setDT(infolong)
 exifdat <- fread("C:/Users/cowl0037/Downloads/Exif_Merge/img_meta.csv")
 #so much info! this is what I decided was important. Includes a ton of time variables and temperature and image name info
 exifsub <- exifdat[,c(1,3,4,7:9,25:37)]
+
 #these subdirectories must've been changed since the upload, so fixing that here.
+# exifsub$SourceFile <- sub("/IMG","/100RECNX/IMG",exifsub$SourceFile)
+# exifsub$SourceFile <- sub("100RECNX/100RECNX","100RECNX",exifsub$SourceFile)
 exifsub$SourceFile <- sub("2018-1-DJFMA/2018-1-Winter-DJF","2018-1-DJFMA",exifsub$SourceFile)
-exifsub$SourceFile <- sub("2018-1-DJFMA/2018-2-Spring-MAM","2018-1-DJFMA",exifsub$SourceFile)
+exifsub$Directory <- sub("2018-1-DJFMA/2018-1-Winter-DJF","2018-1-DJFMA",exifsub$Directory)
+# exifsub$SourceFile <- sub("2018-1-DJFMA/2018-2-Spring-MAM","2018-1-DJFMA",exifsub$SourceFile) #this just isn't in the rename_mapping - must not have been uploaded???
 
 # read in the conversion files that vlad uploaded to MSI. I put them in a folder called exif_merge, and call them by the name pattern, lapply fread and bind them together... i think it works.
 rename_files <-list.files(path="C:/Users/cowl0037/Downloads/Exif_Merge",pattern="image_rename_mapping")
 rename_mapping <- rbindlist(lapply(paste0("C:/Users/cowl0037/Downloads/Exif_Merge/",rename_files),fread))
+rename_mapping$old_path <- sub("2018-1-DJFMAA","2018-1-DJFMA",rename_mapping$old_path)
+rename_mapping$old_path <- sub("2018-1-DJFMA/GridNetwork/C15/20180327/101RECNX","2018-1-DJFMA/GridNetwork/C15/20180327/100RECNX",rename_mapping$old_path)
+rename_mapping$old_path <- sub("2018-1-DJFMA/GridNetwork/C64/20180328/100RECNX","2018-1-DJFMA/GridNetwork/C64/20180328",rename_mapping$old_path)
+rename_mapping$old_path <- sub("2018-2-MJJA/GridNetwork/C100/20810603/100RECNX","2018-2-MJJA/GridNetwork/C100/20810603",rename_mapping$old_path)
+
+
+old_path_path <- strsplit(rename_mapping$old_path,"/IMG")
+old_path_path2 <- strsplit(sapply(old_path_path,`[`,1),"/PICT")
+rename_mapping$Directory <- sapply(old_path_path2,`[`,1)
+
+
+list.exif <- unique(exifsub$Directory[grep("SOND/Grid",exifsub$Directory)])
+list.map <- unique(rename_mapping$Directory[grep("SOND/Grid",rename_mapping$Directory)])
+intersect(list.map,list.exif)
+mapnames <- list.map[!list.map%in%list.exif]
+exifnames <- list.exif[!list.exif%in%list.map]
+for(i in 1:length(mapnames)){
+  rename_mapping$old_path <- sub(mapnames[i],exifnames[i],rename_mapping$old_path)
+}
+
+old_path_path <- strsplit(rename_mapping$old_path,"/IMG")
+old_path_path2 <- strsplit(sapply(old_path_path,`[`,1),"/PICT")
+rename_mapping$Directory <- sapply(old_path_path2,`[`,1)
+
+
+unique(rename_mapping$Directory[!rename_mapping$Directory%in%exifsub$Directory])
+unique(exifsub$Directory[!exifsub$Directory%in%rename_mapping$Directory])
+
 
 #yay data.tables
 setDT(rename_mapping)
@@ -78,6 +110,11 @@ setDT(exifsub)
 #merge 1 - combine the mapping files with exif info by the original path names
 merge1 <- merge(rename_mapping,exifsub,by.x="old_path",by.y="SourceFile")
 
+tail(unique(rename_mapping$old_path[!rename_mapping$old_path%in%merge1$old_path]))
+
+# (rename_mapping$old_path[grep("2018-1-DJFMA/GridNetwork/C14",rename_mapping$old_path)])
+# rename_mapping$old_path[grep("C014",rename_mapping$old_path)]
+# THIS MERGE IS MISSING WHAT IS LABELED SPRING within 2018-1-DJFMA FOLDER AND IT WILL NOT BE EASY TO CORRECT.### 11,000 pictures missing because of this.
 # merge 2 - combine that file with the species info (etc) by the new path
 FINAL <- merge(merge1,infolong,by.x="new_path",by.y="ImageNameWLOCATION")
 
